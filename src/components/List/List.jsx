67b6560b396a673a99921c3d9eac5554a4145b3e
arsen <arsen.dzharullaev26@gmail.com>
2022-05-10 11:14:40 +0300

    final 2.0

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,34 @@
 import {useSelector} from "react-redux";
 import {CoffeeCard} from "../Card/CoffeeCard";
 import  styles from "./List.module.css";
+import {Typography} from "@mui/material";
 
 export const List = () => {
     const {coffee} = useSelector(state => state)
 
     return (
-        <div className={styles.list}>
+        <>
             {
-                coffee.map(item => (
-                    <CoffeeCard
-                        key={item["id"]}
-                        name={item["blend_name"]}
-                        notes={item["notes"]}
-                        origin={item["origin"]}
-                    />))
+                coffee.length > 0
+                    ? <div className={styles.list}>
+                        {
+                            coffee.map(item => (
+                                <CoffeeCard
+                                    key={item["id"]}
+                                    name={item["blend_name"]}
+                                    notes={item["notes"]}
+                                    origin={item["origin"]}
+                                />))
+                        }
+                    </div>
+                    :<div className={styles.empty}>
+                        <Typography variant="h5" gutterBottom component="div">
+                            список кофе пуст(
+                        </Typography>
+                    </div>
             }
-        </div>
+        </>
+
+
     );
 };
